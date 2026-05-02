9833da8f1b5473c4cd94335601fecb6577a3d304
Himanshi Agrawal <agrawalhimanshi4@gmail.com>
2026-05-02 18:37:35 +0000

    datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version of js
+
+//alert(3 + 3) // we are using nodejs not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+let name="Himanshi" //string
+let age=18 //number
+let isLoggedIn=false //boolean
+
+//--Primitive Datatypes--
+//number=> 2 to power 53
+//bigint=> for more bigger values
+//string=> "" or ''
+//boolean=> true/false
+//null=> standalone value(object type)
+// undefined=> value not given
+//symbol=> uniqueness
+
+//--Non- Primitive Datatypes--
+//object
+
+console.log(typeof "Hello") //string type
+console.log(typeof age) //number type
+console.log(typeof undefined); //undefined type
+console.log(typeof null); //object type
